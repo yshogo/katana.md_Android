5ba99b8629f1695a4b4ee1bf6ebf2a58319c375c
shogo.yamada <superman199323@gmail.com>
2017-07-05 16:25:09 +0900

    init commit

--- app/src/main/java/com/example/shogoyamada/katanamd_android/BaseActivity.kt
+++ app/src/main/java/com/example/shogoyamada/katanamd_android/BaseActivity.kt
@@ -1,7 +1,7 @@
 package com.example.shogoyamada.katanamd_android
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class BaseActivity : AppCompatActivity() {
 
